8d335715d564d076bbba01660038b7c68a56fed8
NadirL05 <nadir.lahyani@iae-aix.com>
2025-12-15 14:10:17 +0100

    Update Vite config to use '0.0.0.0' for host

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
-    port: 4173,
+    host: '0.0.0.0',    port: 4173,
     strictPort: true,
+  },
+  server: {
+    host: '0.0.0.0',
   },
 })
